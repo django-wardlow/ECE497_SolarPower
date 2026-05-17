1af67374c8bafd36f2c0a69edc6779fc2cc09e0a
Django <django@inventobot.com>
2026-05-17 12:01:03 -0400

    websight nearly done

--- junior-design-project-team04-amsw/RHcar/main/data/script.js
+++ junior-design-project-team04-amsw/RHcar/main/data/script.js
@@ -1,405 +1,284 @@
 /*
 Author: Django Wardlow
 
-Script file that is responsible for reading data from the Rose-Hulman car and displaying it on the dashboard
+Script file that is responsible for controlling the power supply
 */
 
-//event server based on https://randomnerdtutorials.com/esp32-web-server-gauges/
 
-//adds log entry from the car to the log area on the dashboard
-function add_log_entry(msg){
+let sample = 0;
 
-  console.log(msg);
+//data slots
+graph_data = [
+  [], //sample
+  [], //V out 
+  [], //I out
+  [], //P out
+  [], //V in
+  [], //I in
+  [], //P in
+  [], //efficacy
+  [], //duty
+  [], //load R
+];
 
-  // Create a new <p> element
-  const newParagraph = document.createElement("p");
+let opts = {
+  title: "power supply data",
+  id: "chart1",
+  class: "my-chart",
+  width: window.innerWidth - 20,
+  height: window.innerHeight - 250,
+  scales: {
+    x: {
+      time: false,
+    }
+  },
+  series: [
+    {},
+    {
+      // initial toggled state (optional)
+      show: true,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "V out",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "V",
+  
+      // series style
+      stroke: "green",
+      width: 1,
+      // fill: "rgba(0, 255, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: true,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "I out",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(3) + "A",
+  
+      // series style
+      stroke: "red",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "P out",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(3) + "W",
+  
+      // series style
+      stroke: "orange",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+        {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "V in",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "V",
+  
+      // series style
+      stroke: "purple",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "I in",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "A",
+  
+      // series style
+      stroke: "blue",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "P in",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "W",
+  
+      // series style
+      stroke: "grey",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "efficacy",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "%",
+  
+      // series style
+      stroke: "grey",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "duty",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "%",
+  
+      // series style
+      stroke: "grey",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    },
+    {
+      // initial toggled state (optional)
+      show: false,
+  
+      spanGaps: false,
+  
+      // in-legend display
+      label: "load R",
+      value: (self, rawValue) => rawValue == null ? '' : "" + rawValue.toFixed(2) + "Ω",
+  
+      // series style
+      stroke: "grey",
+      width: 1,
+      // fill: "rgba(255, 0, 0, 0.3)",
+      // dash: [10, 5],
+    }
+  ],
+};
 
-  // Create a new text node
-  const newText = document.createTextNode(msg);
+let uplot = new uPlot(opts, graph_data, document.getElementById("chart-data"));
 
-  // Append the text to the <p> element
-  newParagraph.appendChild(newText);
 
-  // Find the container where text will be added
-  const container = document.getElementById("text-container");
+function clear_graph(){
 
-  // prepend the new <p> element to the container
-  container.prepend(newParagraph);
+  sample = 0;
+
+  graph_data = [
+    [], //sample
+    [], //V out 
+    [], //I out
+    [], //P out
+    [], //V in
+    [], //I in
+    [], //P in
+    [], //efficacy
+    [], //duty
+    [], //load R
+  ];
+
+  uplot.setData(graph_data);
+
 }
+
+
+function isNumeric(str) {
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+
 
 //update the power data on the dashboard
 function update_power_data(data){
 
-  const voltage = document.getElementById("voltage");
+  let checked = document.getElementById("run-graph").checked;
 
-  voltage.textContent = data.voltage + " V";
+  if (isNumeric(pwr_data.voltage) && checked){
 
-  const current = document.getElementById("current");
+    sample += 1;
 
-  current.textContent = data.current + " mA";
+    graph_data[0].push(sample);
+    graph_data[1].push(parseFloat(pwr_data.vout));
+    graph_data[2].push(parseFloat(pwr_data.iout)/1000);
+    graph_data[3].push(parseFloat(pwr_data.pout)/1000);
+    graph_data[4].push(parseFloat(pwr_data.vin));
+    graph_data[5].push(parseFloat(pwr_data.iin)/1000);
+    graph_data[6].push(parseFloat(pwr_data.pin)/1000);
+    graph_data[7].push(parseFloat(pwr_data.eff));
+    graph_data[8].push(parseFloat(pwr_data.duty));
+    graph_data[9].push(parseFloat(pwr_data.rload));
 
-  const available = document.getElementById("available");
 
-  available.textContent = data.available + " J";
+    uplot.setData(graph_data);
+
+  }
+
+  const voltage = document.getElementById("Vout");
+
+  voltage.textContent = data.vout + " V";
+
+  const current = document.getElementById("Iout");
+
+  current.textContent = data.iout + " mA";
+
+  const power = document.getElementById("Pout");
+
+  power.textContent = data.pout + " J";
 
   const watts = document.getElementById("watts");
 
   watts.textContent = data.watts + " mW";
 
-  const energy = document.getElementById("race-energy");
+  const volt_in = document.getElementById("Vin");
 
-  energy.textContent = (data.energy/1000).toFixed(4) + " J used";
+  energy.textContent = (data.vin) + " V";
 
-  const c_energy = document.getElementById("charge-energy");
+  const eff = document.getElementById("Efficiency");
 
-  c_energy.textContent = (data.charged) + " J charged";
+  eff.textContent = (data.eff) + " η";
 
-}
+  const duty = document.getElementById("Duty");
 
-function user_int_changed(index, newval){
-  console.log("int change", index, newval);
-  websocket.send("int,"+index+","+newval);
-}
-
-
-//list of all user ints
-user_ints = [];
-
-function update_user_ints(data){
-
-  //parse ints into array
-  new_user_ints = [];
-
-  updates = data.split(":");
-
-  for (i = 0; i < updates.length; i++){
-    vals = updates[i].split(",");
-
-    user_int = {};
-
-    user_int.name = vals[1];
-
-    //convert string to int
-    user_int.val = Math.round(vals[2]);
-
-    // user_int.focused = false;
-
-    new_user_ints[Math.round(vals[0])] = user_int;
-
-
-  }
-
-  //if any of the ints change rebuild the list
-
-  changed = false;
-
-  //detect if the number of ints changed
-  if(new_user_ints.length != user_ints.length){
-    changed = true;
-  }
-
-  //detect if the names changed
-  else{
-    for(i = 0; i < new_user_ints.length; i++){
-      if(new_user_ints[i].name != user_ints[i].name){
-        changed = true;
-      }
-    }
-  }
-
-  if (changed){
-
-    //remove existing entrys
-
-    let old_inputs = document.getElementById("userint_container");
-
-    if(old_inputs){
-      old_inputs.remove();
-    }
-
-
-    container = document.createElement('div');
-    container.id = "userint_container";
-
-    // Loop to create each input item
-    for (let i = 0; i < new_user_ints.length; i++) {
-      // --- Create the elements for each item ---
-
-      // 1. Create a container div for the label and input for layout
-      const itemDiv = document.createElement('div');
-      itemDiv.className = 'userinputbox'; 
-      itemDiv.id = "userint"
-
-      // 2. Create the label element (the "title")
-      const label = document.createElement('label');
-      const inputId = 'userint' + i; // Create a unique ID for the input
-      label.setAttribute('for', inputId); // Link the label to the input using 'for'
-      label.textContent = new_user_ints[i].name;    // Set the text content of the label
-
-      label.className = 'userinputlabel'; // Fixed width for alignment
-
-      // 3. Create the number input element
-      const input = document.createElement('input');
-      input.setAttribute('type', 'number');
-      input.setAttribute('id', inputId);    // Set the unique ID
-      input.setAttribute('name', 'itemValue' + i); // Optional: Set a name if using in a form
-      input.setAttribute('placeholder', '0'); // Optional: Add a placeholder
-      // input.setAttribute('value', '0'); // Optional: Set a default value
-      // input.setAttribute('min', '0'); // Optional: Set minimum value
-      // input.setAttribute('max', '100'); // Optional: Set maximum value
-
-      //detect when user is editing the input
-      // input.addEventListener('focus', () => {
-      //   new_user_ints[i].focused = true;
-      //   console.log('Editing started');
-      // });
-      
-      // input.addEventListener('blur', () => {
-      //   new_user_ints[i].focused = false;
-      //   console.log('Editing ended');
-      // });
-
-      input.addEventListener('change', () => {
-        user_int_changed(i, input.value);
-      });
-
-      input.className = 'userinput';
-
-      // --- Assemble the item ---
-      itemDiv.appendChild(label); // Add the label to the item's div
-      itemDiv.appendChild(input); // Add the input to the item's div
-
-      // --- Add the complete item to the main container ---
-      container.appendChild(itemDiv);
-
-    }
-
-    parent = document.getElementById("u_ints");
-
-    parent.appendChild(container);
-
-  }
-
-  //update the values of existing ints
-
-  for (let i = 0; i < new_user_ints.length; i++) {
-
-    input = document.getElementById('userint' + i);
-
-    //dont overwright value if the user is typing into the box
-    if(document.activeElement != input){
-      input.value = new_user_ints[i].val;
-    }
-
-  }
-
-
-  user_ints = new_user_ints;
+  duty.textContent = (data.duty) + " %";
 
 }
 
-
-function user_float_changed(index, newval){
-  console.log("float change", index, newval);
-  websocket.send("flt,"+index+","+newval);
+//sends a new value back to the power supply
+function value_changed(element){
+  val = element.value;
+  name = element.id;
+  console.log("setting change", name, val);
+  websocket.send(name+":"+val);
 }
 
 
-//list of all user ints
-user_floats = [];
-
-function update_user_floats(data){
-
-  //parse ints into array
-  new_user_floats = [];
-
-  updates = data.split(":");
-
-  for (i = 0; i < updates.length; i++){
-    vals = updates[i].split(",");
-
-    user_float = {};
-
-    user_float.name = vals[1];
-
-    //convert string to num
-    user_float.val = Number(vals[2]);
-
-    // user_int.focused = false;
-
-    new_user_floats[Math.round(vals[0])] = user_float;
-
-
-  }
-
-  //if any of the ints change rebuild the list
-
-  changed = false;
-
-  //detect if the number of ints changed
-  if(new_user_floats.length != user_floats.length){
-    changed = true;
-  }
-
-  //detect if the names changed
-  else{
-    for(i = 0; i < new_user_floats.length; i++){
-      if(new_user_floats[i].name != user_floats[i].name){
-        changed = true;
-      }
-    }
-  }
-
-  if (changed){
-
-    //remove existing entrys
-
-    let old_inputs = document.getElementById("userfloat_container");
-
-    if(old_inputs){
-      old_inputs.remove();
-    }
-
-
-    container = document.createElement('div');
-    container.id = "userfloat_container";
-
-    // Loop to create each input item
-    for (let i = 0; i < new_user_floats.length; i++) {
-      // --- Create the elements for each item ---
-
-      // 1. Create a container div for the label and input for layout
-      const itemDiv = document.createElement('div');
-      itemDiv.className = 'userinputbox'; 
-      itemDiv.id = "userfloat"
-
-      // 2. Create the label element (the "title")
-      const label = document.createElement('label');
-      const inputId = 'userfloat' + i; // Create a unique ID for the input
-      label.setAttribute('for', inputId); // Link the label to the input using 'for'
-      label.textContent = new_user_floats[i].name;    // Set the text content of the label
-
-      label.className = 'userinputlabel'; // Fixed width for alignment
-
-      // 3. Create the number input element
-      const input = document.createElement('input');
-      input.setAttribute('type', 'number');
-      input.setAttribute('step', '0.00001');
-      input.setAttribute('id', inputId);    // Set the unique ID
-      input.setAttribute('name', 'itemValue' + i); // Optional: Set a name if using in a form
-      input.setAttribute('placeholder', '0'); // Optional: Add a placeholder
-      // input.setAttribute('value', '0'); // Optional: Set a default value
-      // input.setAttribute('min', '0'); // Optional: Set minimum value
-      // input.setAttribute('max', '100'); // Optional: Set maximum value
-
-      //detect when user is editing the input
-      // input.addEventListener('focus', () => {
-      //   new_user_ints[i].focused = true;
-      //   console.log('Editing started');
-      // });
-      
-      // input.addEventListener('blur', () => {
-      //   new_user_ints[i].focused = false;
-      //   console.log('Editing ended');
-      // });
-
-      input.addEventListener('change', () => {
-        user_float_changed(i, input.value);
-      });
-
-      input.className = 'userinput';
-
-      // --- Assemble the item ---
-      itemDiv.appendChild(label); // Add the label to the item's div
-      itemDiv.appendChild(input); // Add the input to the item's div
-
-      // --- Add the complete item to the main container ---
-      container.appendChild(itemDiv);
-
-    }
-
-    parent = document.getElementById("u_floats");
-
-    parent.appendChild(container);
-
-  }
-
-  //update the values of existing ints
-
-  for (let i = 0; i < new_user_floats.length; i++) {
-
-    input = document.getElementById('userfloat' + i);
-
-    //dont overwright value if the user is typing into the box
-    if(document.activeElement != input){
-      input.value = new_user_floats[i].val;
-    }
-
-  }
-
-
-  user_floats = new_user_floats;
-
-}
-
-
-//Sets up the listners for the Server Side Events
-if (!!window.EventSource) {
-  var source = new EventSource('/events');
-  
-  source.addEventListener('open', function(e) {
-    console.log("Events Connected");
-  }, false);
-
-  source.addEventListener('error', function(e) {
-    if (e.target.readyState != EventSource.OPEN) {
-      console.log("Events Disconnected");
-    }
-  }, false);
-  
-  source.addEventListener('message', function(e) {
-    console.log("message", e.data);
-  }, false);
-  
-  //get raw text and send it to add log fn
-  source.addEventListener('log_message', function(e) {
-    console.log("new_log_msg_raw", e.data);
-
-    add_log_entry(e.data);
-
-  }, false);
-
-  //get pwr data json and decode it
-  source.addEventListener('pwr_data', function(e) {
-    // console.log("pwr_data_raw", e.data);
-    var msg_json = JSON.parse(e.data);
-
-    update_power_data(msg_json);
-
-  }, false);
-
-  //get pwr data json and decode it
-  source.addEventListener('user_ints', function(e) {
-    //console.log("user_ints:", e.data);
-
-    update_user_ints(e.data);
-
-  }, false);
-
-  //get pwr data json and decode it
-  source.addEventListener('user_floats', function(e) {
-    //console.log("user_floats:", e.data);
-
-    update_user_floats(e.data);
-    // var msg_json = JSON.parse(e.data);
-
-    // update_power_data(msg_json);
-
-  }, false);
-}
-
-//web sockets used for race buttons and eventually user vars
-// https://randomnerdtutorials.com/esp32-websocket-server-arduino/
+//web sockets connection setup
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 function initWebSocket() {
@@ -421,11 +300,12 @@ function onClose(event) {
 
 //handles messages for the websocket
 function onMessage(event) {
-  //console.log("WS event: ", event.data);
+  console.log("WS event: ", event.data);
 
-  const timer = document.getElementById("timer");
+  var msg_json = JSON.parse(event.data);
 
-  timer.textContent = event.data + " S";
+  update_power_data(msg_json);
+
 }
 
 //init websocket on page load
@@ -436,15 +316,3 @@ function onLoad(event) {
   initWebSocket();
 }
 
-//race button functions
-function start_race_btn(){
-  websocket.send('start');
-}
-
-function manual_charge_btn(){
-  websocket.send('charge');
-}
-
-function stop_race_btn(){
-  websocket.send('stop');
-}
